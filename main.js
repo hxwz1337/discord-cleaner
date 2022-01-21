61b7006e39f47689c4143d0537750df6fa0b5929
Hxwz <97983799+hxwz1337@users.noreply.github.com>
2022-01-21 16:40:06 +0100

    Update main.js

--- main.js
+++ main.js
@@ -56,7 +56,7 @@ _.forEach(a => {
                 })
 
             } else {
-                console.log(`\x1b[32mL'instance ${a.split('/')[5]} is clean!\x1b[0m`)
+                console.log(`\x1b[32mDiscord ${a.split('/')[5]} is clean!\x1b[0m`)
             }
         } else {
             console.log(`\x1b[32mUnable to verify instance ${a.split('/')[5]} because the package.json file does not exist!\x1b[0m`)
